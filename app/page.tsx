9c98837f3d41ade61f87f05e2436d312815598ed
justin <justintrah8@gmail.com>
2026-01-03 14:46:39 +0100

    dictionnary wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import MainSection from "@/components/MainSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-5xl  w-full mx-auto px-6 py-4 ">
+      <MainSection />
       
     </div>
   );
--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -1,13 +1,43 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SearchIcon, Volume2Icon } from "lucide-react";
 
 export default function MainSection() {
   return (
     <section>
       {/* Search Bar */}
       <div>
-        <form action="">
-            <input type="text" n />
-        </form>
+      <form
+  className="
+    relative flex items-center w-full
+    rounded-lg border border-border
+    focus-within:ring-1
+    transition-colors
+  "
+>
+  <Input
+    type="text"
+    placeholder="Search for a word..."
+    className="
+      flex-1 border-0 bg-transparent
+      focus-visible:ring-0
+      focus-visible:ring-offset-0
+      focus:outline-none
+    "
+  />
+
+  <Button
+    type="submit"
+    variant="ghost"
+    size="icon"
+    className="text-muted-foreground hover:text-foreground"
+  >
+    <SearchIcon className="h-5 w-5" />
+    <span className="sr-only">Search</span>
+  </Button>
+</form>
+
       </div>
       {/* Loading */}
       <div></div>
@@ -17,7 +47,38 @@ export default function MainSection() {
       {/*No Results*/}
       <div></div>
       {/* Results */}
-      <div></div>
+      <div>
+       {/* Words*/}
+       <div>
+        {/* Word*/}
+         <div>
+            <h3>{"Word"}</h3>
+         </div>
+         {/* Audio*/}
+         <div>
+            <button>
+                <Volume2Icon />
+            </button>
+            <audio src="" />
+         </div>
+         {/* Phonetic*/}
+         <p>{"Phonetic"}</p>
+       </div>
+       {/* Meaning*/}
+       <div>
+        <h3>{"part of speech"}</h3>
+       </div>
+       <div>
+        <h3>Meanings</h3>
+        {/*List*/}
+        <ul className="">
+            <li className="">
+                <p>definitions</p>
+                <p>Examples:</p>
+            </li>
+        </ul>
+       </div>
+      </div>
     </section>
   );
 }
